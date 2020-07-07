892d991f619311592e7b4e12279afb9800d0e091
csbell-vu <charreau.s.bell@vanderbilt.edu>
2020-07-07 11:25:28 -0500

    created initial analysis script with load and read

---
+++ data_analysis.py
@@ -0,0 +1,15 @@
+#################
+## data_analysis.py
+##
+## Purpose: in this file, I analyze the subject files in data_file.csv
+#################
+
+#imports
+import pandas as pd
+import numpy as np
+from IPython.display import display
+
+#loading function
+data = pd.read_csv('data_file.csv', delimiter='\t')
+print('initial data: ')
+display(data.head())
